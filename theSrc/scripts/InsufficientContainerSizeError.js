c614b410f20e43de9f57e9cc17fe152ab457d9a3
Justin Yap <JustinCCYap@users.noreply.github.com>
2021-09-15 17:18:45 +1000

    Revert "Show original message"
    This reverts commit 12b5d9390e038b83e2cbcc616721c357d0f6fbad.

--- theSrc/scripts/InsufficientContainerSizeError.js
+++ theSrc/scripts/InsufficientContainerSizeError.js
@@ -3,6 +3,7 @@ const INSUFFICIENT_CONTAINER_SIZE_ERROR = 'INSUFFICIENT_CONTAINER_SIZE_ERROR'
 class InsufficientContainerSizeError extends Error {
   constructor () {
     super()
+    this.message = INSUFFICIENT_CONTAINER_SIZE_ERROR
     this.type = INSUFFICIENT_CONTAINER_SIZE_ERROR
   }
 }
